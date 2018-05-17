032d72bc5dbae9e9f3d55a710ff16d4d7e0fbe84
Yunus Emre <Yunus Emre@YunusEmre>
2018-05-18 01:05:34 +0300

    Goodout log request fixed

--- ubs/src/main/java/com/ubs/ubs/controller/GoodoutController.java
+++ ubs/src/main/java/com/ubs/ubs/controller/GoodoutController.java
@@ -1,8 +1,11 @@
 package com.ubs.ubs.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,5 +64,14 @@ public class GoodoutController {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 			
+	}
+	@GetMapping(value = "/insertLogs")
+	public ResponseEntity<List<Goodout>> showLogs(){
+		if(goodoutRepository.count() == 0) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			
+		}else {
+			return new ResponseEntity<List<Goodout>>(goodoutRepository.findAll(),HttpStatus.OK);
+		}
 	}
 }
